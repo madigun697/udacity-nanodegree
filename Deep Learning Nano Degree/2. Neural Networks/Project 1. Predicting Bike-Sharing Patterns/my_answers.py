54c304148dbf4ed36a0f1b8ed478f50b91f3b8e7
Joohyung You <madigun697@gmail.com>
2020-09-28 16:03:52 +0900

    modify hyperparameter for passing the project rubric

--- Deep Learning Nano Degree/2. Neural Networks/Project 1. Predicting Bike-Sharing Patterns/my_answers.py
+++ Deep Learning Nano Degree/2. Neural Networks/Project 1. Predicting Bike-Sharing Patterns/my_answers.py
@@ -91,17 +91,17 @@ class NeuralNetwork(object):
         # TODO: Output error - Replace this value with your calculations.
         error = y - final_outputs # Output layer error is the difference between desired target and actual output.
         # TODO: Calculate the hidden layer's contribution to the error
-        hidden_error = error[:, None].dot(self.weights_hidden_to_output.T)
+        hidden_error = np.dot(self.weights_hidden_to_output, error)
         
         # TODO: Backpropagated error terms - Replace these values with your calculations.
-        output_error_term = error[:, None]
+        output_error_term = error
         
         hidden_error_term = hidden_error * (hidden_outputs * (1 - hidden_outputs))
-        
+  
         # Weight step (input to hidden)
-        delta_weights_i_h += np.dot(X[:, None], hidden_error_term) * self.lr
+        delta_weights_i_h += np.dot(X[:, None], hidden_error_term[:, None].T)
         # Weight step (hidden to output)
-        delta_weights_h_o += np.dot(hidden_outputs[:, None], output_error_term) * self.lr
+        delta_weights_h_o += np.dot(hidden_outputs[:, None], output_error_term[:, None].T)
         return delta_weights_i_h, delta_weights_h_o
 
     def update_weights(self, delta_weights_i_h, delta_weights_h_o, n_records):
@@ -114,8 +114,8 @@ class NeuralNetwork(object):
             n_records: number of records
 
         '''
-        self.weights_hidden_to_output += delta_weights_h_o # update hidden-to-output weights with gradient descent step
-        self.weights_input_to_hidden += delta_weights_i_h # update input-to-hidden weights with gradient descent step
+        self.weights_hidden_to_output += delta_weights_h_o * self.lr # update hidden-to-output weights with gradient descent step
+        self.weights_input_to_hidden += delta_weights_i_h * self.lr # update input-to-hidden weights with gradient descent step
 
     def run(self, features):
         ''' Run a forward pass through the network with input features 
@@ -141,6 +141,6 @@ class NeuralNetwork(object):
 # Set your hyperparameters here
 ##########################################################
 iterations = 1000
-learning_rate = 0.01
-hidden_nodes = 9
+learning_rate = 0.02
+hidden_nodes = 5
 output_nodes = 1
