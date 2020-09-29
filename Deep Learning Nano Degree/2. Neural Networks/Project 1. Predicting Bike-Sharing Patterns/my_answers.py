ccc6e906b88d102d99152fed564277b7b5faadac
Joohyung You <madigun697@gmail.com>
2020-09-30 08:51:06 +0900

    Update project 1

--- Deep Learning Nano Degree/2. Neural Networks/Project 1. Predicting Bike-Sharing Patterns/my_answers.py
+++ Deep Learning Nano Degree/2. Neural Networks/Project 1. Predicting Bike-Sharing Patterns/my_answers.py
@@ -114,8 +114,8 @@ class NeuralNetwork(object):
             n_records: number of records
 
         '''
-        self.weights_hidden_to_output += delta_weights_h_o * self.lr # update hidden-to-output weights with gradient descent step
-        self.weights_input_to_hidden += delta_weights_i_h * self.lr # update input-to-hidden weights with gradient descent step
+        self.weights_hidden_to_output += delta_weights_h_o * (self.lr / n_records) # update hidden-to-output weights with gradient descent step
+        self.weights_input_to_hidden += delta_weights_i_h * (self.lr / n_records) # update input-to-hidden weights with gradient descent step
 
     def run(self, features):
         ''' Run a forward pass through the network with input features 
@@ -140,7 +140,7 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 1000
-learning_rate = 0.02
-hidden_nodes = 5
+iterations = 3000
+learning_rate = 0.7
+hidden_nodes = 20
 output_nodes = 1
